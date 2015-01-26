bd0e12b6ac8701423e98e8a061e423474d32e6c1
jacobdlewis <jacobdlewis@gmail.com>
2015-01-26 14:41:08 -0600

    added some thoroughly 90s effects to the page

--- main.js
+++ main.js
@@ -1,14 +1,51 @@
 document.querySelector('h1').classList.add('red');
 
-//create a variable that holds all h2s. loop through
-//the list and add a class of 'blue' to each item
 var h2s = document.querySelectorAll('h2');
-for (i=0; i<h2s.length; i++) {
-  h2s[i].classList.add('button_like');
+for (i=0; i < h2s.length; i++) {
+  h2s[i].classList.add('blue');
 }
 
+// h3s toUpperCase(); 'hello'.toUpperCase() => 'HELLO'
 var h3s = document.querySelectorAll('h3');
-for (x=0; x<h3s.length; x++) {
-  h3s[x].textContent = h3s[x].textContent.toUpperCase();
+// for (i=0; i < h3s.length; i++) {
+//   h3s[i].textContent = h3s[i].textContent.toUpperCase();
+// }
+
+// [] = Array.prototype
+[].forEach.call(h3s, function(el){
+  el.textContent = el.textContent.toUpperCase();
+})
+
+var target = document.querySelector('.target');
+var elementString = '<h2>So many GOOGs! -  <a href="google.com">GOOG</a></h2>'
+target.innerHTML = elementString;
+
+document.querySelector('#add-element').addEventListener('click', function(){
+  target.insertAdjacentHTML('afterend', elementString);
+});
+
+
+
+
+
+
+
+setInterval(function () {
+  setRandomColor(document.querySelector('h1'))
+}, 1000)
+
+function getRandomColor() {
+  return Math.floor(Math.random() * 256);
+}
+
+function setRandomColor(element){
+  element.style.color = randomColor();
+}
+
+function randomColor() {
+  var red = getRandomColor();
+  var green = getRandomColor();
+  var blue = getRandomColor();
+  return 'rgb(' + red + ',' + green + ',' + blue + ')';
 }
 
